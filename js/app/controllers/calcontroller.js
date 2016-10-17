999cb7a4c644b67ed9b261d168b2115cd045745d
Georg Ehrke <developer@georgehrke.com>
2016-10-17 12:05:58 +0200

    refresh events every 5 minutes

--- js/app/controllers/calcontroller.js
+++ js/app/controllers/calcontroller.js
@@ -26,8 +26,8 @@
 * Description: The fullcalendar controller.
 */
 
-app.controller('CalController', ['$scope', 'Calendar', 'CalendarService', 'VEventService', 'SettingsService', 'TimezoneService', 'VEvent', 'is', 'fc', 'EventsEditorDialogService', 'PopoverPositioningUtility', '$window',
-	function ($scope, Calendar, CalendarService, VEventService, SettingsService, TimezoneService, VEvent, is, fc, EventsEditorDialogService, PopoverPositioningUtility, $window) {
+app.controller('CalController', ['$scope', 'Calendar', 'CalendarService', 'VEventService', 'SettingsService', 'TimezoneService', 'VEvent', 'is', 'fc', 'EventsEditorDialogService', 'PopoverPositioningUtility', '$window', '$interval',
+	function ($scope, Calendar, CalendarService, VEventService, SettingsService, TimezoneService, VEvent, is, fc, EventsEditorDialogService, PopoverPositioningUtility, $window, $interval) {
 		'use strict';
 
 		is.loading = true;
@@ -284,5 +284,9 @@ app.controller('CalController', ['$scope', 'Calendar', 'CalendarService', 'VEven
 					}
 				}
 		};
+
+		$interval(function() {
+			fc.elm.fullCalendar('refetchEvents');
+		}, 300000); //300000 = 1000 * 60 * 5
 	}
 ]);
